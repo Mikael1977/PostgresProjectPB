4165cc7336e88fe19176948a8f3890f5ca40ea63
Mikael1977 <mik_h@mail.ru>
2022-08-30 20:11:22 +0300

    Work 30.08.2022 20:10
    Work 30.08.2022 20:10

--- PurpleBerries.sql
+++ PurpleBerries.sql
@@ -73,8 +73,8 @@ created_at TIMESTAMP
 
 CREATE TABLE baskets_users (
 id SERIAL PRIMARY KEY,
-user_id INT NOT NULL,
-basket_id INT NOT NULL,
+user_id integer REFERENCES users,
+basket_id INT REFERENCES baskets,
 created_at TIMESTAMP
 );
 
@@ -96,6 +96,7 @@ CREATE TABLE orders (
 id SERIAL PRIMARY KEY,
 basket_id INT NOT NULL,
 pickpoint_id INT,
+finish_date TIMESTAMP;
 created_at TIMESTAMP
 );
 
@@ -204,4 +205,71 @@ DROP TRIGGER add_create_at_data_on_insert ON users;
 
 CREATE TRIGGER add_create_at_data_on_insert BEFORE INSERT ON users 
   FOR EACH ROW 
-  EXECUTE FUNCTION add_create_at_data();
+  EXECUTE FUNCTION add_create_at_data
+  
+/*Сложные запросы с использованием подзапросов*/
+
+/*Вывод имени пользователя, адреса доставки и номера заказа за последний месяц с текущего момента с исключением тестовых данных из будущих периодов*/
+
+SELECT 
+id AS order_id, 
+basket_id,
+(SELECT CONCAT(first_name,' ',last_name) FROM users WHERE users.id = 
+(SELECT baskets_users.user_id 
+ FROM baskets_users 
+ WHERE baskets_users.basket_id = orders.basket_id)) AS user_name,
+(SELECT address FROM pickpoints WHERE pickpoints.id = orders.pickpoint_id) AS pickpoint_address
+FROM ORDERS
+WHERE created_at > (now() - interval '1 month') AND created_at <= (now()) AND finish_date IS NULL;
+
+/*Запрос сортирует пункты выдачи по количеству позиций в заказах*/
+
+SELECT 
+address, 
+(SELECT SUM(product_count) FROM baskets_products WHERE basket_id IN 
+ 	(SELECT basket_id FROM orders WHERE pickpoint_id = pickpoints.id)) AS products_count
+FROM pickpoints
+ORDER BY products_count DESC NULLS LAST;
+
+/*Представления*/
+
+/*Представление в том числе выводит информацию о сенах на товар с учетом скидки*/
+
+DROP VIEW IF EXISTS prices_with_discounts; 
+
+CREATE VIEW prices_with_discounts AS
+SELECT 
+products_prices.id AS products_price_id,
+product_id, 
+price, 
+CASE
+	WHEN (products_prices_reduces.percents IS NULL) THEN price
+	ELSE price - price / 100 * products_prices_reduces.percents
+END AS reduced_price,
+products_prices_reduces.percents
+FROM products_prices
+LEFT JOIN products_prices_reduces ON products_prices.id = products_prices_reduces.products_price_id;
+
+SELECT * FROM prices_with_discounts;
+
+/*Функция выводит стоимость товара с учетом индивидуальной скидки. Проверка на существование данных не осуществляется, 
+также заранее должно быть создано представление prices_with_discounts*/
+DROP FUNCTION IF EXISTS get_product_price_with_all_reduces_by_user_id_product_price_id;
+
+CREATE FUNCTION get_product_price_with_all_reduces_by_user_id_product_price_id (user_id INTEGER, product_price_id INTEGER) 
+RETURNS MONEY AS 
+$$
+  SELECT 
+CASE
+	WHEN (prices_with_discounts.percents IS NULL) THEN reduced_price
+	ELSE reduced_price - reduced_price / 100 * products_prices_reduces_individual.percents
+	END
+	FROM products_prices_reduces_individual
+	LEFT JOIN prices_with_discounts ON prices_with_discounts.products_price_id = products_prices_reduces_individual.products_price_id
+	WHERE user_id = user_id AND product_id = product_price_id;
+
+$$ 
+LANGUAGE SQL;
+
+
+SELECT get_product_price_with_all_reduces_by_user_id_product_price_id(171,108);
